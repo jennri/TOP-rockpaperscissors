7779f31bd6b7470d65caff4aadea92000bdcf70e
jenny <jennytran9264@gmail.com>
2023-05-13 00:24:50 +1000

    Round count working.

--- javascript.js
+++ javascript.js
@@ -7,28 +7,35 @@ const choiceBtnScissors = document.querySelector("#scissors");
 const computerScore = document.querySelector(".tally_computer")
 const humanScore = document.querySelector(".tally_human")
 const winner = document.querySelector(".winner")
+const roundText = document.querySelector(".round")
 
+//Rock, Paper, Scissors buttons that gives the gameRoundWinner() it's parameter
 choiceBtnRock.addEventListener('click', () => gameOrder("Rock"))
 choiceBtnPaper.addEventListener('click', () => gameOrder("Paper"))
 choiceBtnScissors.addEventListener('click', () => gameOrder("Scissors"))
 document.getElementById("endGame").style.display = "none";
 
-//Game
+//Declaring global variables to be used in the HTML display
 let computerSelection;
 let result;
 let totalPlayerScore = 0;
 let totalCompScore = 0;
-let round = 0;
+let roundnum = 0;
 
-//Order of Game
+//Order of functions when a round is played. gameRoundWinner() takes computerRandomiser input and player inputs to determine a winner.
+//Winner of the round is displayed.
+//Scores of both computer and player and the rounds are tracked and displayed. 
+//If either score reached 5, endGame() is run.
 function gameOrder(playerSelection){
     computerRandomiser();
     gameRoundWinner(playerSelection)
+    gameRound();
     playerText.textContent = `You: ${playerSelection}`;
     computerText.textContent = `Computer: ${computerSelection}`;
-    computerScore.textContent = `Opponent score count: ${totalCompScore}`
-    humanScore.textContent = `Your score count: ${totalPlayerScore}`
+    computerScore.textContent = `Computer: ${totalCompScore}`
+    humanScore.textContent = `You: ${totalPlayerScore}`
     resultText.textContent = result;
+    roundText.textContent = `Round: ${roundnum}`
     if (totalPlayerScore == 5 || totalCompScore == 5) {
         endGame();
     }
@@ -47,15 +54,20 @@ function computerRandomiser() {
     return computerSelection;
 }
 
-//Declaring round winner
+function gameRound() {
+    roundnum += 1;
+}
+
+//Determining the winner from player and computer inputs using switch cases, comparing the player input to the computers. 
+//Every scenario counts the total scores for both players and returns a string declaring who won. 
 function gameRoundWinner(playerSelection) { 
     if (computerSelection == playerSelection) {
         result = "Tie!"
     }
     if (playerSelection == "Rock") {
         switch (computerSelection) {
-            case "Scissor":
-                result = "You win! Rock beats scissors"
+            case "Scissors":
+                result = "You Win! Rock beats Scissors"
                 totalPlayerScore += 1
                 break;
             case "Paper":
@@ -64,10 +76,10 @@ function gameRoundWinner(playerSelection) {
                 break;
         }
     }
-    if (playerSelection == "Scissors") {
+    else if (playerSelection == "Scissors") {
         switch (computerSelection) {
             case "Rock":
-                result = "You Lose! Rock beats scissors"
+                result = "You Lose! Rock beats Scissors"
                 totalCompScore += 1
                 break;
             case "Paper":
@@ -76,7 +88,7 @@ function gameRoundWinner(playerSelection) {
                 break;
         }
     }
-    if (playerSelection == "Paper") {
+    else if (playerSelection == "Paper") {
         switch (computerSelection) {
             case "Scissors":
                 result = "You Lose! Scissors beats paper"
@@ -88,17 +100,16 @@ function gameRoundWinner(playerSelection) {
                 break;
         }
     }
-
     return result;
 }
 
-//Declaring Winner
+//When either side has 5 points, the buttons are disabled and a pop-up is enabled with a reset button.
 function endGame() {    
     if (totalPlayerScore == 5 ) {
-        winner.textContent = `You won, it's by chance though`
+        winner.textContent = `You defeated the computer!`
     } 
     if (totalCompScore == 5) {
-        winner.textContent = `You Lost...`
+        winner.textContent = `Mission Failed, We'll get them next time...`
     }
     //Disables buttons and pops up the play again button
     choiceBtnRock.setAttribute('disabled', '')
